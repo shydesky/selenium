ddbea4ebb1e7a5ef602da1fe77d911f658e87694
shy <sunhaoyu@papayamobile.cn>
2016-11-23 15:59:24 +0800

    ss

--- selenium/python_org_search.py
+++ selenium/python_org_search.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.PhantomJS()
+service_args = [
+'--proxy=https://neweb.gq:81',
+'--proxy-type=http',
+'--ssl-protocol=any',
+'--ignore-ssl-errors=true'
+]
+driver = webdriver.PhantomJS(service_args=service_args)
 url = 'https://www.youtube.com'
 driver.get(url)
 sss=driver.find_elements_by_class_name('yt-shelf-grid-item')
 print len(sss)
 driver.close()
+
+
+
