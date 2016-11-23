fc30664b53318066cdf9336a17331824da61b608
shy <sunhaoyu@papayamobile.cn>
2016-11-23 11:40:18 +0800

    youtube

--- selenium/python_org_search.py
+++ selenium/python_org_search.py
@@ -2,9 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.PhantomJS()
-driver.get("http://www.shydesky.top")
-pap=driver.find_element_by_id('primary-nav')
-print pap
-sss=driver.find_elements_by_class_name('entry-excerpt')
-import pdb;pdb.set_trace()
+url = 'https://www.youtube.com'
+driver.get(url)
+sss=driver.find_elements_by_class_name('yt-shelf-grid-item')
+print len(sss)
 driver.close()
